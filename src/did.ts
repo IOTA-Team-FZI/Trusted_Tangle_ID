8d8ffe95f59018f26d1cd51be847c34c266fb9a2
Robin Lamberti <lamberti@fzi.de>
2019-11-04 15:10:18 +0100

    cleanup

--- src/did.ts
+++ src/did.ts
@@ -1,28 +1,39 @@
-import { Trytes } from "@iota/core/typings/types";
-import { publishDid } from "./tangleConnector";
-import { DidDocument } from "./types";
-import { API } from "@iota/core";
+import { Trytes } from '@iota/core/typings/types';
+import { publishDid } from './tangleConnector';
+import { DidDocument } from './types';
+import { API } from '@iota/core';
 import * as Mam from '@iota/mam';
 import elliptic from 'elliptic';
 import { createHash } from 'crypto';
 
-export const DEFAULT_PROVIDER = "https://nodes.devnet.thetangle.org:443";
+export const DEFAULT_PROVIDER = 'https://nodes.devnet.thetangle.org:443';
 export const METHOD_NAME = '';
 
+const ec = new elliptic.ec('curve25519');
+
 export default class DID {
-  constructor(private readonly seed: Trytes, public document: DidDocument, private mamChannel: Mam.MamState) {}
+  constructor(public document: DidDocument, private readonly seed: Trytes, private keyPair: elliptic.ec.KeyPair, private mamChannel: Mam.MamState) {}
 
   static fromSeed(seed: Trytes, provider = DEFAULT_PROVIDER) {
     const channel = Mam.init(provider, seed);
     const root = Mam.getRoot(channel);
-    const ec = new elliptic.ec('curve25519');
+    
     const secret = createHash('sha256').update(seed).digest('hex');
     const keyPair = ec.keyFromPrivate(secret);
     const document: DidDocument = {
-      "@context": 'https://www.w3.org/2019/did/v1',
+      '@context': 'https://www.w3.org/2019/did/v1',
       id: `did:${METHOD_NAME}:${root}`,
       publicKey: keyPair.getPublic('hex')
     }
-    return new DID(seed, document, channel)
+    return new DID(document, seed, keyPair, channel)
+  }
+
+  async sync() {
+    const result = await Mam.fetch(Mam.getRoot(this.mamChannel), 'public');
+    if (result instanceof Error) {
+
+    } else {
+
+    }
   }
 }
