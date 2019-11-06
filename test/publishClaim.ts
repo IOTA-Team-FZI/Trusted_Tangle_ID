7155d0e5c07937a57849602e9c4dec060bc4003a
Robin Lamberti <lamberti@fzi.de>
2019-11-06 16:31:43 +0100

    try to read claim

--- test/publishClaim.ts
+++ test/publishClaim.ts
@@ -1,14 +1,29 @@
 import DID from "../src/did";
 import {generate} from 'randomstring';
+import { composeAPI } from "@iota/core";
+import { trytesToAscii } from "@iota/converter";
 
 const seed = generate({ length: 81, charset: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ9' });
 
 const did = DID.fromSeed(seed);
 
-console.log(did)
+// console.log(did)
 
 const claim = did.createClaim(did.getMethodSpecificIdentifier(), 'eClass:manufacturer')
-console.log(claim)
+// console.log(claim)
 DID.publishClaim(claim).then((value) => {
   console.log(value)
-});
+
+  const iota = composeAPI({
+    provider: 'https://nodes.devnet.thetangle.org:443'
+  })
+  setTimeout(() => {
+    iota.getBundle(value[0].hash).then((bundle: any) => {
+      const result = trytesToAscii(bundle[0].message)
+      console.log(result)
+    }).catch((err: any) => {
+      console.error(err)
+    })
+  }, 2000);
+  
+}).catch((err: any) => console.error(err));
